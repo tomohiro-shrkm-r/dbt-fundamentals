916c5abd9a22148040bbb292b84917aa575b8e71
shrkm-t <shira0613@gmail.com>
2022-03-06 15:24:52 +0000

    refactor stripe files

--- models/staging/jaffle_shop/stg_customers.sql
+++ models/staging/jaffle_shop/stg_customers.sql
@@ -1,11 +1,11 @@
 with customers as (
 
-    select
-        id as customer_id,
-        first_name,
-        last_name
-
-    from {{  source('jaffle_shop', 'customers')}}
+select
+    id as order_id,
+    user_id as customer_id,
+    order_date,
+    status
+from {{ source('jaffle_shop', 'orders') }}
 
 )
 
